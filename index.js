a25846ab4080e9d6cda6ca62cc827dc9b9f3b14e
hossain <hossainahamedk@gmail.com>
2024-04-29 22:35:48 +0600

    fix import issue

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require('express')
+const app = express()
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require("dotenv").config();
-const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
